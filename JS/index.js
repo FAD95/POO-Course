6b4bb6d2ce5169045b4feee010c049ee72fe7af9
FAD95 <36573399+FAD95@users.noreply.github.com>
2020-10-30 17:12:26 -0500

    Implementacion con js

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+var car = new Car("AMQ123", new Account("Fadith Escorcia","123456789"))
+car.passenger=4
+car.printDataCar()
+
+var car2 = new Car("QWE258", new Account("Camila Troncoso","123456789"))
+car2.passenger=4
+car2.printDataCar()
