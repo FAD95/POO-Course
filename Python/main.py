42d4492edc06482a5306be0173be8f916cf39451
FAD95 <36573399+FAD95@users.noreply.github.com>
2020-10-30 21:14:01 -0500

    Implementacióon con Python

--- Python/main.py
+++ Python/main.py
@@ -1,16 +1,13 @@
 from car import Car
+from account import Account
 
 if __name__=="__main__":
-    car = Car()
+    car = Car("AMQ123", Account("Fadith Escorcia","123456789"))
     car.id=1
-    car.license="AMQ123"
-    car.driver="Fadith Escorcia"
     car.passenger=4
     print(vars(car))
 
-    car2 = Car()
+    car2 = Car("QWE258",Account("Camila Troncoso","123456789"))
     car2.id=2
-    car2.license="QWE258"
-    car2.driver="Camila Troncoso"
     car2.passenger=5
     print(vars(car2))
