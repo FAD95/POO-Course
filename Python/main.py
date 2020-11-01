2fc92ba1da2e170a0f2a74332008f7bc7226e010
FAD95 <36573399+FAD95@users.noreply.github.com>
2020-11-01 11:00:31 -0500

    Herencia con python

--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,15 @@
 from car import Car
 from account import Account
+from uberX import UberX
+from uberPool import UberPool
 
 if __name__=="__main__":
-    car = Car("AMQ123", Account("Fadith Escorcia","123456789"))
+    car = uberX("AMQ123", Account("Fadith Escorcia","123456789"), "Chevrolet", "Spark")
     car.id=1
     car.passenger=4
     print(vars(car))
 
-    car2 = Car("QWE258",Account("Camila Troncoso","123456789"))
+    car2 = Car("QWE258",Account("Camila Troncoso","123456789"),"Hyundai","Elantra")
     car2.id=2
     car2.passenger=5
     print(vars(car2))
