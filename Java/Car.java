2a9050d31a8de968735fb0658b508eeae9bc887c
FAD95 <36573399+FAD95@users.noreply.github.com>
2020-11-01 14:57:02 -0500

    Polimosrfismo con Java

--- Java/Car.java
+++ Java/Car.java
@@ -1,9 +1,9 @@
 package Java;
 
 class Car {
-    Integer id;
-    String license;
-    Account driver;
+    private Integer id;
+    private String license;
+    private Account driver;
     private Integer passenger;
 
     public Car(String license, Account driver){
@@ -28,4 +28,30 @@ class Car {
             System.out.println("El auto debe tener 4 pasajeros");
         }
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public Account getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Account driver) {
+        this.driver = driver;
+    }
+
+    
 }
--- Java/Main.java
+++ Java/Main.java
@@ -6,13 +6,11 @@ package Java;
 class Main {
     public static void main(String[] args) {
         UberX car = new UberX("AMQ123", new Account("Fadith Escorcia", "123456789"), "Chevrolet", "Spark");
-        car.id=1;
         car.setPassenger(4);
         car.printDataCar();
 
-        UberPool car2 = new UberPool("QWE258", new Account("Camila Troncoso", "123456789"),"Hyundai","Elantra");
-        car2.id=2;
-        car2.setPassenger(4);
+        UberVan car2 = new UberVan("QWE258", new Account("Camila Troncoso", "123456789"));
+        car2.setPassenger(6);
         car2.printDataCar();
     }    
 }
